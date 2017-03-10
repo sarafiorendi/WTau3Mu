2501f056615fbef631478e23931340eaadb57787
Riccardo Manzoni <riccardo.manzoni@cern.ch>
2017-03-10 18:30:53 +0100

    add kinematic fitter calss

--- src/classes.h
+++ src/classes.h
@@ -2,9 +2,11 @@
 
 #include "DataFormats/Common/interface/Wrapper.h"
 #include "CMGTools/WTau3Mu/interface/Tau3MuKalmanVertexFitter.h"
+#include "CMGTools/WTau3Mu/interface/Tau3MuKinematicVertexFitter.h"
 
 namespace {
   struct CMGTools_WTau3Mu {
-    Tau3MuKalmanVertexFitterProducer tau3muvtx_;
+    Tau3MuKalmanVertexFitterProducer tau3muKalmanVtx_;
+    Tau3MuKinematicVertexFitterProducer tau3muKinVtx_;
   };
 }

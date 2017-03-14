cb890d007d1f71092d9eaecadc193641976dd831
Riccardo Manzoni <riccardo.manzoni@cern.ch>
2017-03-14 15:18:47 +0100

    add 03Feb2017 data samples

--- python/samples/data_2016.py
+++ python/samples/data_2016.py
@@ -20,7 +20,7 @@ DoubleMuonLowMass_Run2016H_PromptReco_v1 = kreator.makeDataComponent("DoubleMuon
 DoubleMuonLowMass_Run2016H_PromptReco_v2 = kreator.makeDataComponent("DoubleMuonLowMass_Run2016H_PromptReco_v2", "/DoubleMuonLowMass/Run2016H-PromptReco-v2/MINIAOD", "CMS", ".*root", json)
 DoubleMuonLowMass_Run2016H_PromptReco_v3 = kreator.makeDataComponent("DoubleMuonLowMass_Run2016H_PromptReco_v3", "/DoubleMuonLowMass/Run2016H-PromptReco-v3/MINIAOD", "CMS", ".*root", json)
 
-datasamplesDoubleMuLowMass = [
+datasamplesDoubleMuLowMassSept23 = [
     DoubleMuonLowMass_Run2016B_23Sep2016    ,
     DoubleMuonLowMass_Run2016C_23Sep2016    ,
     DoubleMuonLowMass_Run2016D_23Sep2016    ,
@@ -30,4 +30,26 @@ datasamplesDoubleMuLowMass = [
     DoubleMuonLowMass_Run2016H_PromptReco_v1,
     DoubleMuonLowMass_Run2016H_PromptReco_v2,
     DoubleMuonLowMass_Run2016H_PromptReco_v3,
+]
+
+DoubleMuonLowMass_Run2016Bv1_03Feb2017     = kreator.makeDataComponent("DoubleMuonLowMass_Run2016Bv1_03Feb2017"    , "/DoubleMuonLowMass/Run2016B-03Feb2017_ver1-v1/MINIAOD" , "CMS", ".*root", json)
+DoubleMuonLowMass_Run2016Bv2_03Feb2017     = kreator.makeDataComponent("DoubleMuonLowMass_Run2016Bv2_03Feb2017"    , "/DoubleMuonLowMass/Run2016B-03Feb2017_ver2-v2/MINIAOD" , "CMS", ".*root", json)
+DoubleMuonLowMass_Run2016C_03Feb2017       = kreator.makeDataComponent("DoubleMuonLowMass_Run2016C_03Feb2017"      , "/DoubleMuonLowMass/Run2016C-03Feb2017-v1/MINIAOD"      , "CMS", ".*root", json)
+DoubleMuonLowMass_Run2016D_03Feb2017       = kreator.makeDataComponent("DoubleMuonLowMass_Run2016D_03Feb2017"      , "/DoubleMuonLowMass/Run2016D-03Feb2017-v1/MINIAOD"      , "CMS", ".*root", json)
+DoubleMuonLowMass_Run2016E_03Feb2017       = kreator.makeDataComponent("DoubleMuonLowMass_Run2016E_03Feb2017"      , "/DoubleMuonLowMass/Run2016E-03Feb2017-v1/MINIAOD"      , "CMS", ".*root", json)
+DoubleMuonLowMass_Run2016F_03Feb2017       = kreator.makeDataComponent("DoubleMuonLowMass_Run2016F_03Feb2017"      , "/DoubleMuonLowMass/Run2016F-03Feb2017-v1/MINIAOD"      , "CMS", ".*root", json)
+DoubleMuonLowMass_Run2016G_03Feb2017       = kreator.makeDataComponent("DoubleMuonLowMass_Run2016G_03Feb2017"      , "/DoubleMuonLowMass/Run2016G-03Feb2017-v1/MINIAOD"      , "CMS", ".*root", json)
+DoubleMuonLowMass_Run2016Hv2_03Feb2017     = kreator.makeDataComponent("DoubleMuonLowMass_Run2016Hv2_03Feb2017"    , "/DoubleMuonLowMass/Run2016H-03Feb2017_ver2-v1/MINIAOD" , "CMS", ".*root", json)
+DoubleMuonLowMass_Run2016Hv3_03Feb2017     = kreator.makeDataComponent("DoubleMuonLowMass_Run2016Hv3_03Feb2017"    , "/DoubleMuonLowMass/Run2016H-03Feb2017_ver3-v1/MINIAOD" , "CMS", ".*root", json)
+
+datasamplesDoubleMuLowMass = [
+    DoubleMuonLowMass_Run2016Bv1_03Feb2017,
+    DoubleMuonLowMass_Run2016Bv2_03Feb2017,
+    DoubleMuonLowMass_Run2016C_03Feb2017  ,
+    DoubleMuonLowMass_Run2016D_03Feb2017  ,
+    DoubleMuonLowMass_Run2016E_03Feb2017  ,
+    DoubleMuonLowMass_Run2016F_03Feb2017  ,
+    DoubleMuonLowMass_Run2016G_03Feb2017  ,
+    DoubleMuonLowMass_Run2016Hv2_03Feb2017,
+    DoubleMuonLowMass_Run2016Hv3_03Feb2017,
 ]

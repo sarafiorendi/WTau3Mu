93b1066a9382c51f5c835767b744988a006acca9
Riccardo Manzoni <riccardo.manzoni@cern.ch>
2017-10-13 15:50:57 +0200

    scripts to add freshly bdt classificator branches ex post to existing ntuples

---
+++ bdt/add_new_bdt_background.py
@@ -0,0 +1,104 @@
+import ROOT
+import numpy as np
+import xgboost as xgb
+import pandas, root_numpy
+from sklearn.externals import joblib
+from sklearn import preprocessing
+import matplotlib.pyplot as plt 
+
+ifile = 'data2016v4.root'
+
+file = ROOT.TFile.Open(ifile, 'read')
+file.cd()
+tree = file.Get('tree')
+
+classifier = joblib.load('classifier_new.pkl')
+
+feat_names =[
+    'cand_refit_mttau', 
+    'cand_refit_mass12', 
+    'cand_refit_mass13', 
+    'cand_refit_mass23', 
+    'cand_refit_tau_pt', 
+    'cand_refit_tau_eta', 
+    'cand_refit_dRtauMET', 
+    'cand_refit_dPhitauMET', 
+    'cand_refit_dRtauMuonMax', 
+    'cand_refit_tau_dBetaIsoCone0p8strength0p2_rel',
+    'tau_sv_prob', 
+    'tau_sv_ls', 
+    'tau_sv_cos',
+    'mu1_refit_reliso05', 
+    'mu2_refit_reliso05', 
+    'mu3_refit_reliso05', 
+    'mu1_refit_muonid_BDT',
+    'mu2_refit_muonid_BDT',
+    'mu3_refit_muonid_BDT',
+    'n_taus',
+    'n_muons',
+    'n_electrons',
+    'mu1_jet_pt', 
+    'mu2_jet_pt', 
+    'mu3_jet_pt', 
+    'mu1_jet_eta', 
+    'mu2_jet_eta', 
+    'mu3_jet_eta', 
+    'mu1_jet_mass', 
+    'mu2_jet_mass', 
+    'mu3_jet_mass', 
+    'mu1_jet_charge', 
+    'mu2_jet_charge', 
+    'mu3_jet_charge', 
+    'mu1_refit_pt', 
+    'mu2_refit_pt', 
+    'mu3_refit_pt', 
+    'cand_refit_met_pt',
+    'abs(cand_refit_me_eta_1)+abs(cand_refit_me_eta_2)',
+    'cand_refit_me_eta_1',
+    'cand_refit_me_eta_2',
+    'cand_refit_mez_1',
+    'cand_refit_mez_2',
+    'cand_refit_w_isgood',
+    'cand_refit_w_pt',
+    'HTjets',
+    'HTbjets',
+    'njets',
+    'nbjets',
+    'cand_refit_charge12',
+    'cand_refit_charge13',
+    'cand_refit_charge23',
+]
+
+
+print 'loading support dataset...'
+dataset_support = pandas.DataFrame(
+    root_numpy.root2array(
+        ifile,
+        'tree',
+        branches=feat_names,
+    )
+)
+print '\t...done'
+
+print 'loading dataset...'
+dataset = pandas.DataFrame(
+    root_numpy.root2array(
+        ifile,
+        'tree',
+    )
+)
+print '\t...done'
+
+print 'computing probabilities...'
+bdt_proba_v2_array = classifier.predict_proba(dataset_support)[:,1]
+print '\t...done'
+
+
+print 'adding new column to the dataset...'
+dataset['bdt_proba_v2'] = bdt_proba_v2_array
+print '\t...done'
+
+# you'll love it https://github.com/scikit-hep/root_pandas
+import root_pandas
+dataset.to_root('data2016v4_enriched.root', key='tree', store_index=False)
+

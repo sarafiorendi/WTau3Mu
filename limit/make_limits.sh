fbb23a592a109f90a74d61489e1052e7335b1f4f
Riccardo Manzoni <riccardo.manzoni@cern.ch>
2018-04-26 14:59:14 +0200

    more on limits

--- limit/make_limits.sh
+++ limit/make_limits.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-wps='0.65 0.70 0.75 0.80 0.82 0.84 0.86 0.88 0.89 0.90 0.91 0.92 0.94'
+# wps='0.65 0.70 0.75 0.80 0.82 0.84 0.86 0.88 0.89 0.90 0.91 0.92 0.94'
+wps='0.89'
 for wp in $wps
     do
     echo "WP "$wp
@@ -15,7 +16,7 @@ for wp in $wps
     text2workspace.py $datacard -o $model 
     
 #     echo 'computing the 95% limit'
-    combine -M HybridNew --testStat=LHC --frequentist $model -T 2000 --expectedFromGrid 0.5 &> central_0.95_WP$wp.txt
+#     combine -M HybridNew --testStat=LHC --frequentist $model -T 2000 --expectedFromGrid 0.5 &> central_0.95_WP$wp.txt
 #     combine -M HybridNew --testStat=LHC --frequentist $model -T 2000 --expectedFromGrid 0.16 &> plus_one_sigma_0.95_WP$wp.txt
 #     combine -M HybridNew --testStat=LHC --frequentist $model -T 2000 --expectedFromGrid 0.84 &> minus_one_sigma_0.95_WP$wp.txt
     
--- limit/prepare_card.py
+++ limit/prepare_card.py
@@ -16,9 +16,10 @@ import os
 from math import pi, sqrt
 from glob import glob
 from pdb import set_trace
+from array import array 
 
-ROOT.gROOT.SetBatch()
-ROOT.gStyle.SetOptStat(False)
+# ROOT.gROOT.SetBatch()
+ROOT.gStyle.SetOptStat(True)
 ROOT.TH1.SetDefaultSumw2()
  
 ##########################################################################################
@@ -87,6 +88,104 @@ for fname in data_files:
 # create output file
 output = ROOT.TFile.Open('datacard%s.root' %args.category, 'recreate')
 
+# dump data
+print 'getting data'
+selected_tree = data_tree.CopyTree('(' + args.selection + ') & cand_refit_tau_mass>1.61 & cand_refit_tau_mass<1.99')
+
+# fix variable name
+newtree = ROOT.TTree('newtree','newtree')
+imass = array( 'f', [ 0 ] )
+newtree.Branch('mass', imass, 'mass/F')
+
+sara = ROOT.TCanvas()
+for event in selected_tree:
+    imass[0] = event.cand_refit_tau_mass
+    newtree.Fill()
+
+mass_histo_data = ROOT.TH1F('mass_histo_data', 'mass_histo_data', 40, 1.6, 2)
+newtree.Draw('mass >> mass_histo_data', 'mass<1.72 | mass > 1.84')
+
+def fline(x, par):
+    if reject and x[0] > 1.72 and x[0] < 1.84:
+        ROOT.TF1.RejectPoint()
+        return 0
+    return par[0] + par[1]*x[0];
+
+retta = ROOT.TF1('retta', fline, 1.61, 1.99, 2)
+retta.SetParameters(0,0)
+retta.SetLineColor(ROOT.kBlue)
+reject = True
+fit_result_data = getattr(mass_histo_data.Fit(retta, 'RSIQL'), 'Get()', mass_histo_data.Fit(retta, 'RSIQL')) # we want a likelihood fit because we want to count empty bins too
+retta.Draw('SAMEL')
+slope = fit_result_data.Parameter(1)
+ROOT.gPad.Update()
+
+sara.SaveAs('bkg%s.pdf'%(args.category))
+
+# final histo
+
+ROOT.gStyle.SetOptStat(0)
+mycanvas = ROOT.TCanvas('mycanvas', '', 700, 700)
+mycanvas.cd()
+retta_tot = ROOT.TF1('fl', 'pol1', 1.61, 1.99, 2)
+retta_tot.SetParameters(retta.GetParameters())
+
+# trapezoid area under the pol1
+def integ(mm, qq, xmin, xmax, binwidth=0.01):
+    return (0.5 * mm * (xmax**2 - xmin**2) + qq*(xmax -xmin))/binwidth
+
+qq = retta.GetParameter(0)
+mm = retta.GetParameter(1)
+total_bkg = integ(mm, qq, 1.61, 1.99)
+
+# import pdb ; pdb.set_trace()
+
+mass_histo_data.GetListOfFunctions().Remove(mass_histo_data.GetListOfFunctions()[0]) # ROOT, what's not to love?
+
+mass_histo_data.SetTitle(args.category)
+mass_histo_data.GetXaxis().SetTitle('m_{3#mu} [GeV]')
+mass_histo_data.GetYaxis().SetTitle('events / 10 MeV')
+mass_histo_data.GetYaxis().SetTitleOffset(1.4)
+
+mass_histo_data.SetLineColor   (ROOT.kBlack)
+mass_histo_data.SetMarkerColor(ROOT.kBlack)
+mass_histo_data.SetMarkerStyle(8)
+mass_histo_mc  .SetLineColor(ROOT.kRed  )
+mass_histo_mc  .SetFillColor(ROOT.kRed  )
+mass_histo_mc  .SetFillStyle(3004)
+
+mass_histo_data.Draw('EP')
+mass_histo_mc  .Draw('HISTSAME')
+retta_tot.SetLineColor(ROOT.kBlack)
+retta_tot.Draw('SAMEL')
+gaus.Draw('SAMEL')
+
+combo = ROOT.TF1('combo', 'pol1(0) + gaus(2)', 1.61, 1.99)
+combo.SetLineColor(ROOT.kGray+1)
+combo.SetLineStyle(2)
+combo.SetParameter(0, retta.GetParameter(0))
+combo.SetParameter(1, retta.GetParameter(1))
+combo.SetParameter(2, gaus.GetParameter(0))
+combo.SetParameter(3, gaus.GetParameter(1))
+combo.SetParameter(4, gaus.GetParameter(2))
+combo.Draw('SAMEL')
+
+mycanvas.Update()
+
+mycanvas.SaveAs('final_mass_plot_%s.pdf'%(args.category))
+
+ROOT.gStyle.SetOptStat(True)
+
+mass = ROOT.RooRealVar('mass','mass',1.61,1.99)
+
+print 'dumping data'
+data =  ROOT.RooDataSet(
+    'data_obs', 
+    'data_obs',
+    newtree, 
+    ROOT.RooArgSet(mass)
+)
+
 # create workspace
 print 'creating workspace'
 w = ROOT.RooWorkspace('t3m_shapes')
@@ -97,14 +196,8 @@ w = ROOT.RooWorkspace('t3m_shapes')
 
 # first order poly
 w.factory('mass[%f, %f]' % (1.61, 1.99))
-w.factory('a0%s[0]' %(args.category))
+w.factory('a0%s[%.3f]' %(args.category, max(slope, 0.)))
 w.factory('RooPolynomial::bkg(mass,{a0%s})' %args.category)
-
-# second order poly
-# w.factory('mass[%f, %f]' % (1.61, 1.99))
-# w.factory('a0%s[3]' %(args.category))
-# w.factory('a1%s[0]' %(args.category))
-# w.factory('RooPolynomial::bkg(mass,{a0%s, a1%s})' %(args.category, args.category))
 
 # dump signal with fixed shape (the final fit will only vary the normalisation
 w.factory('mean[%f]'  % mean)
@@ -117,20 +210,6 @@ all_vars = [it.Next() for _ in range( w.allVars().getSize())]
 for var in all_vars:
     if var.GetName() in ['mean', 'sigma']:
         var.setConstant()
-
-# dump data
-print 'getting data'
-selected_tree = data_tree.CopyTree('(' + args.selection + ') & cand_refit_tau_mass>1.61 & cand_refit_tau_mass<1.99')
-
-print 'dumping data'
-data =  ROOT.RooDataSet(
-    'data_obs', 
-    'data_obs',
-    selected_tree, 
-    ROOT.RooArgSet(w.var('mass'))
-)
-
-# import pdb ; pdb.set_trace()
 
 getattr(w,'import')(data)
 w.Write()
@@ -155,14 +234,14 @@ observation       -1
 bin                                     Wtau3mu{cat}        Wtau3mu{cat}
 process                                 signal              background
 process                                 0                   1
-rate                                    {signal:.4f}            {bkg:.4f}
+rate                                    {signal:.4f}        {bkg:.4f}
 --------------------------------------------------------------------------------
 bkgNorm{cat}  lnU                       -                   4.00
 a0{cat}       flatParam
 lumi          lnN                       1.025               -   
 xs_W          lnN                       1.05                -   
 br_Wtaunu     lnN                       1.0021              -   
-mc_stat       lnN                       {mcstat:.4f}        -   
+mc_stat{cat}  lnN                       {mcstat:.4f}        -   
 mu_id{cat}    lnN                       {mu_id:.4f}         -   
 mu_hlt{cat}   lnN                       {mu_hlt:.4f}        -   
 trk_hlt{cat}  lnN                       {trk_hlt:.4f}       -   
@@ -171,60 +250,13 @@ hlt_extrap    lnN                       1.05                -
 '''.format(
          cat    = args.category,
          signal = sqrt(2.*pi) * sigma * amplitude / 0.01, # area under the normal divided by the bin width (10 MeV)
-         bkg    = data.numEntries(),
+         bkg    = total_bkg, #data.numEntries(),
          mcstat = 1. + sqrt(mass_histo_mc.Integral()/args.signalnorm)/mass_histo_mc.Integral()*args.signalnorm,
-         mu_id  = 0.044  if 'barrel' in args.category else 0.078,
-         mu_hlt = 0.012  if 'barrel' in args.category else 0.040,
-         trk_hlt= 0.0086 if 'barrel' in args.category else 0.0086,
+         mu_id  = 1.044  if 'barrel' in args.category else 1.078,
+         mu_hlt = 1.012  if 'barrel' in args.category else 1.040,
+         trk_hlt= 1.0086 if 'barrel' in args.category else 1.0086,
          )
 )
 
 
 # import pdb ; pdb.set_trace()
-
-# 
-# 
-# 
-# # dump the datacard
-# with open('datacard%s.txt' %args.category, 'w') as card:
-#    card.write(
-# '''
-# imax 1 number of bins
-# jmax * number of processes minus 1
-# kmax * number of nuisance parameters
-# --------------------------------------------------------------------------------
-# shapes background    Wtau3mu{cat}       datacard{cat}.root t3m_shapes:bkg
-# shapes signal        Wtau3mu{cat}       datacard{cat}.root t3m_shapes:sig
-# shapes data_obs      Wtau3mu{cat}       datacard{cat}.root t3m_shapes:data_obs
-# --------------------------------------------------------------------------------
-# bin               Wtau3mu{cat}
-# observation       -1
-# --------------------------------------------------------------------------------
-# bin                                     Wtau3mu{cat}        Wtau3mu{cat}
-# process                                 signal              background
-# process                                 0                   1
-# rate                                    {signal:.4f}            {bkg:.4f}
-# --------------------------------------------------------------------------------
-# bkgNorm{cat}  lnU                       -                   4.00
-# a0{cat}       flatParam
-# a1{cat}       flatParam
-# lumi          lnN                       1.025               -   
-# xs_W          lnN                       1.01                -   
-# br_Wtaunu     lnN                       1.0021              -   
-# mc_stat       lnN                       {mcstat:.4f}        -   
-# mu_id{cat}    lnN                       {mu_id:.4f}         -   
-# mu_hlt{cat}   lnN                       {mu_hlt:.4f}        -   
-# trk_hlt{cat}  lnN                       {trk_hlt:.4f}       -   
-# hlt_extrap    lnN                       1.05                -   
-# --------------------------------------------------------------------------------
-# '''.format(
-#          cat    = args.category,
-#          signal = sqrt(2.*pi) * sigma * amplitude / 0.01, # area under the normal divided by the bin width (10 MeV)
-#          bkg    = data.numEntries(),
-#          mcstat = 1. + sqrt(mass_histo_mc.Integral()/args.signalnorm)/mass_histo_mc.Integral()*args.signalnorm,
-#          mu_id  = 0.044  if args.category=='barrel' else 0.078,
-#          mu_hlt = 0.012  if args.category=='barrel' else 0.040,
-#          trk_hlt= 0.0086 if args.category=='barrel' else 0.0086,
-#          )
-# )
-# 
